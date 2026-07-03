f02a4bb7c810aa856c6350d05328d9be3a98d388
ritoban-oss <ritoban@nutgraf.agency>
2026-07-03 07:16:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,4 @@
 //
 // ─────────────────────────────────────────────────────────────────────────────
 
-const SCRIPT_URL = 'PASTE_YOUR_APPS_SCRIPT_URL_HERE';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzfufxvaD5qS3ZoQqMLFeZGdkjF_bVHc7zty1iM0Bk2Eqn6imnDws9ob6ZUbqse4sTpJw/exec';
